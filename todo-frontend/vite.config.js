6a6416a2c16a0c3e50fd6c09829334f39797145d
Gwapobenjie <plazabenjie1@gmail.com>
2025-04-07 11:52:49 +0800

    removed readme

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-frontend/", 
+  base: "/backend/todo-frontend/",
   server: {
     proxy: {
       "/api": {
@@ -14,4 +14,8 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'build', 
+  },
 });
+
